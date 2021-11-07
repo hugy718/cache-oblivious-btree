193eabff2881a9561a5186dd1728123e0cc1c573
hugy718 <hugy718@gmail.com>
2021-11-07 16:15:09 +0800

    Add block tranfer counting at Cache

--- include/cache.h
+++ include/cache.h
@@ -32,7 +32,8 @@ class CacheBlock {
 class Cache {
  public:
   Cache() = delete;
-  Cache(uint64_t size) : size_(size), usage_(0) {}
+  Cache(uint64_t size) : size_(size), usage_(0), 
+    block_transfer_count_(0) {}
   ~Cache() = default;
 
   void Add(const std::string& id, char* src, uint64_t len);
@@ -43,11 +44,25 @@ class Cache {
   
   char* Get(const std::string& id) { return contents_[id].data(); }
 
+  // inform cache the block size to count number of transfer
+  // the content added to cache can be multiple block size
+  inline void set_block_size_for_stats(uint64_t block_size) {
+    block_transfer_size_=block_size;
+  } 
+
+  // output the counted block transfer
+  inline uint64_t recorded_block_transfer() const { return block_transfer_count_; }
+
+  // reset the counted block transfer to 0
+  inline void reset_block_transfer_stats() { block_transfer_count_ = 0; }
+
  private:
   const uint64_t size_; // M bytes
   uint64_t usage_; // bytes used
   std::map <std::string, CacheBlock> contents_;
   std::list<std::string> fifo_list_; // can be extended to other replacement policy
+  uint64_t block_transfer_size_; // block size for us to count block transfer
+  uint64_t block_transfer_count_; // +1 when a block sized content added to/evicted from cache
 };
 
 }  // namespace cobtree
--- src/cache.cc
+++ src/cache.cc
@@ -8,13 +8,16 @@ void Cache::Add(const std::string& id, char* src, uint64_t len) {
   if (Exist(id)) return;
   while (usage_ + len > size_) {
     const std::string& block_to_delete = fifo_list_.front();
-    usage_ -= contents_[block_to_delete].len();
+    auto deleted_size = contents_[block_to_delete].len();
+    usage_ -= deleted_size;
+    block_transfer_count_ += (deleted_size - 1) / block_transfer_size_ + 1;
     contents_.erase(block_to_delete);    
     fifo_list_.pop_front();
   }
 
   fifo_list_.push_back(id);
   contents_[id].FillContent(src, len);
+  block_transfer_count_ += (len - 1) / block_transfer_size_ + 1;
   usage_ += len;
 }
 
